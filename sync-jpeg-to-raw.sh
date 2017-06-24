b57364d5f12fb2f172738e539651b7be33a2367f
Thomas Karmann <thomas@krmnn.de>
2017-06-24 12:32:40 +0200

    * correctly cd into folders containing spaces * RAW -> RAF

--- sync-jpeg-to-raw.sh
+++ sync-jpeg-to-raw.sh
@@ -3,11 +3,17 @@
 trash_cmd="$(pwd)/trash -v "
 
 for folder in "$@"; do
-    echo "Cleaning folder '$folder'"
-    cd $folder
 
-    if [ -d "./RAW" ]; then
-        find ./RAW -iname "*.raf" -exec sh -c 'if [ ! -f ./JPG/$(basename -s .raf {}).jpg ]; then echo {}; fi' \; | xargs $trash_cmd
+    if [ -d "${folder}" ]; then
+        echo "Cleaning folder '${folder}'"
+        cd "${folder}"
+    else
+        echo "Folder '${folder}' is not accessible! Skipping..."
+        continue
+    fi
+
+    if [ -d "./RAF" ]; then
+        find ./RAF -iname "*.raf" -exec sh -c 'if [ ! -f ./JPG/$(basename -s .raf {}).jpg ]; then echo {}; fi' \; | xargs $trash_cmd
 
     elif [ -d "./NEF" ]; then
         find ./NEF -iname "*.nef" -exec sh -c 'if [ ! -f ./JPG/$(basename -s .nef {}).jpg ]; then echo {}; fi' \; | xargs $trash_cmd
